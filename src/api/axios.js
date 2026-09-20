ea4eb03c687b22a25ffb02bb37edc2a94b944a9e
yashG0 <yashgaurkar0@gmail.com>
2026-09-20 09:36:01 +0530

    Restore request interceptor for JWT attachment (add response interceptor for 401 handling)

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,14 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 api.interceptors.response.use(
   (response) => response,
   (error) => {
