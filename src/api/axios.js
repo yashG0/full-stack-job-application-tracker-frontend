2d3ad6ddfa80bd9fa3372aafaea80e451a45584a
yashG0 <yashgaurkar0@gmail.com>
2026-09-20 09:35:31 +0530

    Redirect to login on 401 instead of showing a generic error

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,12 +4,16 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default api;
